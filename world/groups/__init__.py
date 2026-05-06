497761a220fff02aa0f7f911873492cddf1678f8
Kaygiff <kaygiffens@gmail.com>
2026-05-06 05:40:47 +0500

    Update __init__.py

--- world/groups/__init__.py
+++ world/groups/__init__.py
@@ -1,2 +1,2 @@
-from world.groups.moderation import warn_user_in_group, ban_user_in_group, mute_user_in_group, kick_user_from_group
+from world.groups.moderation import warn_user_in_group, ban_user_in_group, mute_user_in_group
 from world.groups.stats import get_group_stats
